0a494f3f56375247c370e610498ed5d3451a7d30
Smitha6 <smitha6@g.cofc.edu>
2018-08-31 08:33:03 -0400

    Drag/Drop

--- Assets/Code/Item.cs
+++ Assets/Code/Item.cs
@@ -5,35 +5,70 @@ using UnityEngine;
 public class Item : MonoBehaviour
 {
     Vector3 mousePosition;
-    Vector3 snapSize = new Vector3(1, 1, 1);
+    int snapSize = 5;
     Vector3 startPos;
 
     // Use this for initialization
     void Start() {
         startPos = this.gameObject.transform.position;
+        Debug.Log("START");
     }
 
     // Update is called once per frame
     void Update() {
 
-        Vector3 i = this.gameObject.transform.position.Distance;
+
 
     }
 
     // Moves button to position
     private void OnMouseDrag(){
+        Debug.Log("IN");
         mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         transform.position = Vector2.Lerp(transform.position, mousePosition, 10);
     }
 
+    /*
     private void OnMouseUp(){
-        //this.gameObject.transform.position = startPos;
 
-        if (Vector3.Distance(this.gameObject.transform.position, Vector3){
+        //get closest cell
+        GameObject cell = FindClosestCell();
 
+
+        //get its center
+        Vector3 center = cell.transform.position;
+
+        //move this to cells center if its close enough
+        if (Vector3.Distance(center, this.gameObject.transform.position) <= snapSize){
+            this.transform.position = center;
+        }
+        else{
+            this.gameObject.transform.position = startPos;
         }
         
     }
+
+    // Finds the closest cell
+    public GameObject FindClosestCell()
+    {
+        GameObject[] gos;
+        gos = GameObject.FindGameObjectsWithTag("cell");
+        GameObject closest = null;
+        float distance = Mathf.Infinity;
+        Vector3 position = transform.position;
+        foreach (GameObject go in gos)
+        {
+            Vector3 diff = go.transform.position - position;
+            float curDistance = diff.sqrMagnitude;
+            if (curDistance < distance)
+            {
+                closest = go;
+                distance = curDistance;
+            }
+        }
+        return closest;
+    }
+    */
 
 
 }
