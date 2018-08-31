c66755cd02d878cf69906c85266f2fe4311f8a08
Smitha6 <smitha6@g.cofc.edu>
2018-08-31 08:15:56 -0400

    Added drag/drop prefabs

---
+++ Assets/Code/Cell.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : MonoBehaviour {
+    Transform itemT;
+    GameObject item;
+
+	// Use this for initialization
+	void Start () {
+        //gets child item
+        if (this.gameObject.transform.childCount > 0){
+            itemT = gameObject.transform.GetChild(0);
+            item = itemT.gameObject;
+        }
+        
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+
+		
+	}
+
+
+
+
+
+
+}
---
+++ Assets/Code/Item.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    Vector3 mousePosition;
+    Vector3 snapSize = new Vector3(1, 1, 1);
+    Vector3 startPos;
+
+    // Use this for initialization
+    void Start() {
+        startPos = this.gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        Vector3 i = this.gameObject.transform.position.Distance;
+
+    }
+
+    // Moves button to position
+    private void OnMouseDrag(){
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = Vector2.Lerp(transform.position, mousePosition, 10);
+    }
+
+    private void OnMouseUp(){
+        //this.gameObject.transform.position = startPos;
+
+        if (Vector3.Distance(this.gameObject.transform.position, Vector3){
+
+        }
+        
+    }
+
+
+}
